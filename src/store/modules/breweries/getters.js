7d62fb1eed475142d693d08a025c5178fa8ea838
Dmitriy <dimaprostokosmos@gmail.com>
2023-01-12 10:43:47 +0200

    a

--- src/store/modules/breweries/getters.js
+++ src/store/modules/breweries/getters.js
@@ -14,24 +14,103 @@ export default {
   getCitiesList({ breweriesList }) {
     const arrayCity = breweriesList.map((item) => item.city);
     return arrayCity
-      .filter((item,pos) => arrayCity.indexOf(item) === pos)
+      .filter((item, pos) => arrayCity.indexOf(item) === pos)
       .sort();
   },
   getStatesList({ breweriesList }) {
     const arrayState = breweriesList.map((item) => item.state);
     return arrayState
-      .filter((item,pos) => arrayState.indexOf(item) === pos)
+      .filter((item, pos) => arrayState.indexOf(item) === pos)
       .sort();
   },
   getMap({ activeBrewery }) {
     return activeBrewery;
   },
 };
+// function getAverage(marks) {
+//   let res = 0;
+//   for (let i = 0; i < marks.length; i++) {
+//     res = (res + marks[i]);
+//     console.log(res);
+//   }
+//   return Math.floor(res / marks.length);
+// }
+// console.log (getAverage([1,2,3,4,5,6,7,9,7,7,560456,45645,6546,66,6]));
 
+// function fixTheMeerkat(arr) {
+//   let res = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     res = arr[i] + ',' + res;
+//     console.log(res);
+//   }
+//   return res.split(',');
+// }
 
-function addLength(str) { 
- return str.split(' ').map( item => item +' '+ item.length);
+// function fixTheMeerkat(arr) {
+//   let res =[];
+//   for (let i = 0; i < arr.length; i++) {
+//     res.unshift(arr[i]);
+//   }
+//   return res;
+// }
+// console.group(fixTheMeerkat(["ground","rainbow","sky"]));
+
+//  function fixTheMeerkat(arr) {
+//   return arr.reduce(function (accumulator, currentValue) {
+//     accumulator.unshift(currentValue);
+//     return accumulator;
+//   }, []);
+// }
+
+//  function fixTheMeerkat(arr) {
+//    const res = arr.reverse();
+//    return res;
+//  }
+
+// function fixTheMeerkat(arr) {
+//   const res = [];
+//   arr.forEach((element) => {
+//     res.unshift(element);
+//   });
+//   return res;
+// }
+
+// console.log(fixTheMeerkat(['ground', 'rainbow', 'sky']));
+
+// function monkeyCount(n) {
+//   let res = [];
+
+//   for (let i = 0; i < n; i++) {
+//     res.push(i+1);
+//   }
+//   return res;
+// }
+// console.log(monkeyCount(7));
+
+// const flip = (d, a) => {
+//   if (d === 'R') {
+//     return a.sort((a, b) => a - b);
+//   } else {
+//     return a.sort((a,b) => b - a);
+//   }
+// };
+
+// console.log(flip('R', [3, 2, 1, 2,33]));
+// console.log(flip('L', [1, 4, 5, 3, 5,44]));
+
+function smaller(numbers) {
+  const res = [];
+  for (let i = 0; i < numbers.length; i++) {
+    let counter = 0;
+    for (let c = i + 1; c < numbers.length; c++) {
+      if (numbers[i] > numbers[c]) {
+        counter++;
+      }
+    }
+    res.push(counter);
+  }
+  return res;
 }
 
 
-console.log(addLength('you will win'));
+console.log(smaller([5, 4, 7, 9, 2, 4, 4, 5, 6])); //expecting  [4, 1, 5, 5, 0, 0, 0, 0, 0])
--- src/store/modules/breweries/index.js
+++ src/store/modules/breweries/index.js
@@ -11,43 +11,43 @@ export default {
   mutations,
 };
 
-const numbersArray = [1, 7, 2, 4, 6, 9, 3, 12, 4, 7, 9, 0, 4, 9, 3, 5, 8];
-const match_1 = [4, 6, 9];
-const match_2 = [4, 9, 3];
+// const numbersArray = [1, 7, 2, 4, 6, 9, 3, 12, 4, 7, 9, 0, 4, 9, 3, 5, 8];
+// const match_1 = [4, 6, 9];
+// const match_2 = [4, 9, 3];
 
-function checkMatch(target, match) {
-  let res = false;
+// function checkMatch(target, match) {
+//   let res = false;
 
-  for (let i = 0; i < target.length; i++) {
-    if (target[i] === match[0]) {
-      res = true;
-      console.log('alert', target[i]);
-      for (let c = 1; c < match.length; c++) {
-        console.log('match[c]', c, match[c]);
-        console.log('target[c + i]', c + i, target[c + i]);
-        console.log('-------------------');
-        if (match[c] !== target[c + i]) {
-          res = false;
-        }
-      }
-    }
-  }
-  return res;
-}
+//   for (let i = 0; i < target.length; i++) {
+//     if (target[i] === match[0]) {
+//       res = true;
+//       console.log('alert', target[i]);
+//       for (let c = 1; c < match.length; c++) {
+//         console.log('match[c]', c, match[c]);
+//         console.log('target[c + i]', c + i, target[c + i]);
+//         console.log('-------------------');
+//         if (match[c] !== target[c + i]) {
+//           res = false;
+//         }
+//       }
+//     }
+//   }
+//   return res;
+// }
 
-function checkMatch_2(target, match) {
-  const targetString = target.join('');
-  const matchString = match.join('');
+// function checkMatch_2(target, match) {
+//   const targetString = target.join('');
+//   const matchString = match.join('');
 
-  console.log(targetString);
-  console.log(matchString);
-  console.log(targetString.includes(matchString));
+//   console.log(targetString);
+//   console.log(matchString);
+//   console.log(targetString.includes(matchString));
 
-  return targetString.includes(matchString);
-}
+//   return targetString.includes(matchString);
+// }
 
-const isMatchExist = checkMatch(numbersArray, match_1);
-console.log('==>', isMatchExist);
+// const isMatchExist = checkMatch(numbersArray, match_1);
+// console.log('==>', isMatchExist);
 
-const isMatchExist_2 = checkMatch_2(numbersArray, match_2);
-console.log('2 ==>', isMatchExist_2);
+// const isMatchExist_2 = checkMatch_2(numbersArray, match_2);
+// console.log('2 ==>', isMatchExist_2);
