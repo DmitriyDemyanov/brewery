0e1f314e74860fb4c3a6ef334e09a85d37feadae
Dmitry Demyanov <dimaprostokosmos@gmail.comgit config --global user.email dimaprostokosmos@gmail.com>
2023-01-16 21:12:21 +0200

    working to makeup

---
+++ src/components/MakeupItem.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="wrapper-item d-flex justify-content-around">
+    <div class="card" style="width: 18rem">
+      <img
+        src="https://static.insales-cdn.com/files/1/660/10781332/original/PHOTO-2019-12-09-19-44-18-2.jpg"
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+      </ul>
+      <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+    <div class="card" style="width: 18rem">
+      <img
+        src="https://static.insales-cdn.com/files/1/660/10781332/original/PHOTO-2019-12-09-19-44-18-2.jpg"
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+      </ul>
+      <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+    <div class="card" style="width: 18rem">
+      <img
+        src="https://static.insales-cdn.com/files/1/660/10781332/original/PHOTO-2019-12-09-19-44-18-2.jpg"
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+      </ul>
+      <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+      </div>
+    </div>
+
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MakeUpItem',
+};
+</script>
+
+<style lang="scss" scoped>
+
+.card {
+  opacity: 0.4;
+}
+
+</style>
--- src/pages/MakeupPage.vue
+++ src/pages/MakeupPage.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="main-page">
-    <router-link class="p-2" to="/"> Main Page </router-link>
-    <div class="text-center">
-      <span class="title">hello fashion</span>
+    <div class="main-bg">
+      
+      <router-link class="p-2" to="/"> Main Page </router-link>
+
+      <nav class="d-flex justify-content-around">
+        <label class="product-text" for="product-type"
+          ><span class="title">hello fashion</span></label
+        >
+        <select
+          name="product-type"
+          class="form-select"
+          aria-label="Default select example"
+        >
+          <option selected>All</option>
+          <option value="blush">Blush</option>
+          <option value="bronzer">Bronzer</option>
+          <option value="eyebrow">Eyebrow</option>
+          <option value="eyeliner">Eyeliner</option>
+          <option value="eyeshadow">Eyeshadow</option>
+          <option value="foundation">Foundation</option>
+          <option value="lip_liner">Lip liner</option>
+          <option value="lipstick">Lipstick</option>
+          <option value="mascara">Mascara</option>
+          <option value="nail_polish">Nail polish</option>
+        </select>
+      </nav>
+      <MakeUpItem />
     </div>
   </div>
 </template>
 
 <script>
+import MakeUpItem from '@/components/MakeupItem';
+
 export default {
   name: 'MakeupPage',
+  components: {
+    MakeUpItem,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.form-select {
+  width: 20%;
+  background: linear-gradient(45deg, gold, #1613a9);
+  &:focus {
+    box-shadow: none;
+  }
+}
+.main-bg {
+  height: 100vw;
 
+  background-image: url("https://thumbs.dreamstime.com/z/gold-woman-skin-beauty-fashion-model-girl-golden-makeup-hair-jewellery-black-background-87310205.jpg");
+  background-repeat: no-repeat;
+}
 </style>
